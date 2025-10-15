a25c0be03c6a0275ef354b5a21910ca298cc5df8
illfiction <atharva.sonwane@outlook.com>
2025-10-15 16:00:50 +0530

    time over ground station

--- 3d-simulations/main.py
+++ 3d-simulations/main.py
@@ -1,18 +1,22 @@
 import numpy as np
 from satellite import Satellite
 from visualize import plot_orbit
-from constants import EARTH_MU, J
+from time_over_ground_station import time_over_ground_station
+from constants import EARTH_MU, J ,EARTH_RADIUS
 
 def run_simulation():
-    initial_position = np.array([-4579.5, -4097.12, 3296.86])
-    initial_velocity = np.array([-1.866, -3.234, -6.571])
+    # initial_position = np.array([-4579.5, -4097.12, 3296.86])
+    # initial_velocity = np.array([-1.866, -3.234, -6.571])
+
+    initial_position = np.array([6828.1, 0, 0])
+    initial_velocity = np.array([0, 6.629, 3.824])
     initial_quaternion = np.array([0.58,0.58,0,0.5720139858])
     initial_angular_velocity = np.array([0, 0, 0.0])
 
     satellite = Satellite(initial_position, initial_velocity, initial_quaternion, initial_angular_velocity)
 
-    time_span = 1 * 24 * 60 * 3.95  # For 1 SSO orbit
-    dt = 1
+    time_span = 90 * 24 * 60 * 60
+    dt = 100
     steps = int(time_span / dt)
 
     state_list = []
@@ -30,7 +34,10 @@ def run_simulation():
         # energy_list.append(energy)
 
     states = np.array(state_list)
+
+    time_over_ground_station(states, dt)
     plot_orbit(states, quaternion_list)
+
 
 
 if __name__ == '__main__':
